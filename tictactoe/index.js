b46a2ab8a95725af6bf901758d5a274b3eca6b36
Tatituri <vtatituri@wpi.edu>
2025-07-16 10:13:33 -0400

    d

--- tictactoe/index.js
+++ tictactoe/index.js
@@ -3,19 +3,17 @@ const keypadButtons = document.querySelectorAll("#container .cell");
 
 // Clear the display initially so we can start fresh
 callCenterDisplay.innerHTML = "";
-let counter=0
 // Add a click handler to each button
 keypadButtons.forEach(button => {
     button.addEventListener("click", () => {
-        counter = 0
         const digit = button.innerHTML.trim();
         callCenterDisplay.innerHTML += digit;
+        
+        
+        console.log(digit);
+        digitlist = []
+        digitlist.push(digit)
+        console.log(digitlist)
     });
 });
 
-console.log(callCenterDisplay.length);
-
-
-const element = document.getElementById('myElement');
-const textLength = element.innerText.length; // Or element.innerText.length for older IE
-console.log(`Text content length: ${textLength}`);
